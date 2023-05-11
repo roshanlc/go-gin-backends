0382dd1d1914edbbb32100a3cd3d60985ae6ba46
Roshan Lamichhane <lamichhanersn@gmail.com>
2023-05-11 11:38:03 +0545

    02-basic-rest-api: added more endpoints

--- 02-basic-rest-api/main.go
+++ 02-basic-rest-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,7 @@ import (
 	"github.com/rs/xid"
 )
 
+// data storage
 var recipes []models.Recipe
 
 func init() {
@@ -16,6 +18,7 @@ func init() {
 	recipes = make([]models.Recipe, 0)
 }
 
+// handler for POST Method to "/recipes" endpoint
 func NewRecipeHandler(c *gin.Context) {
 	var recipe models.Recipe
 	if err := c.ShouldBindJSON(&recipe); err != nil {
@@ -35,13 +38,97 @@ func NewRecipeHandler(c *gin.Context) {
 
 }
 
+// handler for GET Method to "/recipes" endpoint
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
+}
+
+// handler for PUT Method to "/recipes/:id" endpoint
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe models.Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "malformed JSON: " + err.Error(),
+		})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes[index] = recipe
+	// assign the old id
+	recipe.ID = id
+	// assign the current time as it was updated
+	recipe.PublishedAt = time.Now()
+	c.JSON(http.StatusOK, recipe)
+}
+
+// handler for DELETE Method to "/recipes/:id" endpoint
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted",
+	})
+}
+
+// handler for GET method to "/recipes/search?tag=" endpoint
+func SearchHandler(c *gin.Context) {
+	tag := c.Query("tag")
+
+	recipesList := make([]models.Recipe, 0)
+
+	for i := 0; i < len(recipes); i++ {
+		// break on first find
+		found := false
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
+			}
+		}
+
+		if found {
+			recipesList = append(recipesList, recipes[i])
+		}
+	}
+
+	c.JSON(http.StatusOK, recipesList)
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
-	router.GET("/", ListRecipesHandler)
+	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchHandler)
 	router.Run(":9000")
 }
